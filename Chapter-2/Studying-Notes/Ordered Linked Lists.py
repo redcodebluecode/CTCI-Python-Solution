a72c2c90282a6a872fa24cd144857d68dcec890b
Steward Landcast <student.linguistics@gmail.com>
2014-02-03 13:44:49 -0500

    Create Ordered Linked Lists.py

---
+++ Chapter-2/Studying-Notes/Ordered Linked Lists.py
@@ -0,0 +1,215 @@
+"""
+The Ordered List Abstract Data Type
+The structure of an ordered list is a collection of items where each item holds a relative position with respect to the others. Some possible ordered list operations are given below. As we consider the operations for the ordered list, we should note that some methods can be implemented the same as with unordered lists since they deal only with the number of nodes in the list without regard to the actual item values. 
+
+List() creates a new list that is empty. It needs no parameters and returns an empty list.
+
+prl() print all the items in the list. It needs no parameters and prints all the items in the list. If the list is empty, it will print "Empty".
+
+remove(item) removes the item from the list. It needs the item and modifies the list. Assume the item is present in the list.
+
+isEmpty() tests to see whether the list is empty. It needs no parameters and returns a boolean value.
+
+size() returns the number of items in the list. It needs no parameters and returns an integer.
+
+index(item) returns the position of item in the list. It needs the item and returns the index. Assume the item is in the list.
+
+pop() removes and returns the last item in the list. It needs nothing and returns an item. Assume the list has at least one item.
+
+pop(pos) removes and returns the item at position pos. It needs the position and returns the item. Assume the item is in the list.
+
+The two remaining methods, add and search, will require some modification.
+
+add(item) adds a new item to the list. It needs the item and returns nothing. Assume the item is not already in the list.
+
+search(item) searches for the item in the list. It needs the item and returns a boolean value.
+"""
+
+##################################
+## Step 1: Create a Node class
+##################################
+
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+        
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setData(self, newdata):
+        self.data = newdata
+    
+    def setNext(self, newnext):
+        self.next = newnext
+
+print "Test Step 1:"
+testnode = Node(17)
+print "Create a new Node and get its data:", 
+print testnode.getData()
+print ""
+
+##########################################
+## Step 2: Create an Ordered List class
+##########################################
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+###########################################
+## Step 3: Create List operations
+###########################################
+# add(item): adds a new item to the list. It needs the item and returns nothing. Assume the item is not already in the list.
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+# search(self, item): searches for the item in the list. It needs the item and returns a boolean value.
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+# prl(self): print all the items in the list
+    def prl(self):
+        if self.head == None:
+            print "Empty"
+        else:
+            current = self.head
+            while current != None:
+                print current.getData()
+                current = current.getNext()
+
+# isEmpty(): tests to see whether the list is empty. It needs no parameters and returns a boolean value.
+    def isEmpty(self):
+        return self.head == None
+
+# size(): returns the number of items in the list. It needs no parameters and returns an integer.
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+# remove(self, item): removes the item from the list. It needs the item and modifies the list. Assume the item is present in the list.
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+            
+
+# index(self, item): returns the position of item in the list. It needs the item and returns the index. Assume the item is in the list.
+    def index(self, item):
+        ind = 0
+        current = self.head
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+                return ind
+            else:
+                current = current.getNext()
+                ind += 1
+
+# pop(self): removes and returns the last item in the list. It needs nothing and returns an item. Assume the list has at least one item.
+    def pop(self):
+        current = self.head
+        previous = None
+        if current.getNext() == None:
+            self.head = None
+        else:
+            while current.getNext() != None:
+                previous = current
+                current = current.getNext()
+            previous.setNext(None)
+        return current.getData()
+        
+# poppos(self, pos): removes and returns the item at position pos. It needs the position and returns the item. Assume the item is in the list.
+    def poppos(self, pos):
+        ind = 0
+        current = self.head
+        previous = None
+        if pos == 0:
+            self.head = current.getNext()
+        else:
+            while ind < pos:
+                ind += 1
+                previous = current
+                current = current.getNext()
+            previous.setNext(current.getNext())
+        return current.getData()
+
+#################################
+## Test
+#################################
+
+print "Test Step 2:"
+print "Create a new unorderedlist:"
+mylist = OrderedList()
+print ""
+
+print "Test Step 3:"
+print "Test whether the list is empty:", mylist.isEmpty()
+
+print "Add items to the list:"
+mylist.add(16)
+mylist.add(26)
+mylist.add(35)
+mylist.add(99)
+mylist.add(12)
+mylist.add(49)
+mylist.prl()
+
+print "Test whether the list is empty:", mylist.isEmpty()
+
+print "Returns the number of items in the list", mylist.size()
+
+print "Search whether an item is in the list:"
+print(mylist.search(99))
+print(mylist.search(18))
+
+print "Remove an item:"
+print(mylist.size())
+mylist.prl()
+mylist.remove(99)
+print(mylist.search(99))
+print(mylist.size())
+mylist.prl()
+
+print "Find the index of an item in the list:"
+print mylist.index(0)
+print mylist.index(1)
+print mylist.index(100)
+
+print "Pop the last item in the list:"
+mylist.prl()
+mylist.pop()
+mylist.prl()
+
+print "Pop an item at a designated position:"
+mylist.prl()
+mylist.poppos(0)
+mylist.poppos(1)
+mylist.prl()
